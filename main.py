c3899ff454922f735241cac622284116562cedf1
Kacper Bardecki <44558448+WackoKacko@users.noreply.github.com>
2023-08-29 16:09:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-# import nau7802py, time
+import nau7802py, time
 
-# myScale = nau7802py.NAU7802() # Create instance of the NAU7802 class
+myScale = nau7802py.NAU7802() # Create instance of the NAU7802 class
 
-# #
-# # Begin void setup() equivalent
-# #
+#
+# Begin void setup() equivalent
+#
 
-# print('Qwiic Scale Example')
+print('Qwiic Scale Example')
 
-# if not myScale.begin():
-#     print('Scale not detected. Please check wiring. Freezing...')
-#     while True:
-#         pass
+if not myScale.begin():
+    print('Scale not detected. Please check wiring. Freezing...')
+    while True:
+        pass
   
-# print('Scale detected!')
+print('Scale detected!')
 
-# myScale.setLDO(nau7802py.NAU7802_LDO_Values['NAU7802_LDO_4V5']) #AVDD can be set to 2.4V, 2.7V, 3.0V, 3.3V, 3.6V, 3.9V, 4.2V, or 4.5V. Voltae supply to LDO must be at least 0.3V greater than selected AVDD!
+myScale.setLDO(nau7802py.NAU7802_LDO_Values['NAU7802_LDO_4V5']) #AVDD can be set to 2.4V, 2.7V, 3.0V, 3.3V, 3.6V, 3.9V, 4.2V, or 4.5V. Voltae supply to LDO must be at least 0.3V greater than selected AVDD!
 
-# myScale.setGain(nau7802py.NAU7802_Gain_Values['NAU7802_GAIN_128']) # Gain can be set to 1, 2, 4, 8, 16, 32, 64, or 128.
+myScale.setGain(nau7802py.NAU7802_Gain_Values['NAU7802_GAIN_128']) # Gain can be set to 1, 2, 4, 8, 16, 32, 64, or 128.
 
-# myScale.setSampleRate(nau7802py.NAU7802_SPS_Values['NAU7802_SPS_10']) # Sample rate can be set to 10, 20, 40, 80, or 320Hz
+myScale.setSampleRate(nau7802py.NAU7802_SPS_Values['NAU7802_SPS_10']) # Sample rate can be set to 10, 20, 40, 80, or 320Hz
 
-# myScale.calibrateAFE() # Does an internal calibration. Recommended after power up, gain changes, sample rate changes, or channel changes.
+myScale.calibrateAFE() # Does an internal calibration. Recommended after power up, gain changes, sample rate changes, or channel changes.
 
-# #
-# # Begin void loop() equivalent
-# #
+#
+# Begin void loop() equivalent
+#
 
-# while True:
-#     if myScale.available():
-#         currentReading = myScale.getReading();
-#         print('Reading: ', currentReading)
+while True:
+    if myScale.available():
+        currentReading = myScale.getReading();
+        print('Reading: ', currentReading)
 
 
 #
