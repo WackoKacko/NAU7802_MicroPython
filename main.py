aa62e320a1fe90f3e75042d2879cd430b92ff703
Kacper Bardecki <kbardecki@gmail.com>
2023-08-29 15:14:35 +0100

    more detailed example

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import nau7802py, time
 
-myScale = nau7802py.NAU7802()
+myScale = nau7802py.NAU7802() # Create instance of the NAU7802 class
 
-if myScale.begin():
+#
+# Begin void setup() equivalent
+#
+
+print('Qwiic Scale Example')
+
+if not myScale.begin():
+    print('Scale not detected. Please check wiring. Freezing...')
     while True:
-        currentReading = myScale.getReading()
-        print('Reading: ' + str(currentReading))
-        time.sleep(2)
-else:
-    print("Something went wrong")
+        pass
+  
+print('Scale detected!')
+
+myScale.setLDO(nau7802py.NAU7802_LDO_Values['NAU7802_LDO_4V5']) #AVDD can be set to 2.4V, 2.7V, 3.0V, 3.3V, 3.6V, 3.9V, 4.2V, or 4.5V. Voltae supply to LDO must be at least 0.3V greater than selected AVDD!
+
+myScale.setGain(nau7802py.NAU7802_Gain_Values['NAU7802_GAIN_2']) # Gain can be set to 1, 2, 4, 8, 16, 32, 64, or 128.
+
+myScale.setSampleRate(nau7802py.NAU7802_SPS_Values['NAU7802_SPS_40']) # Sample rate can be set to 10, 20, 40, 80, or 320Hz
+
+myScale.calibrateAFE() # Does an internal calibration. Recommended after power up, gain changes, sample rate changes, or channel changes.
+
+#
+# Begin void loop() equivalent
+#
+
+while True:
+    if myScale.available():
+        currentReading = myScale.getReading();
+        print('Reading: ', currentReading)
+        time.sleep(0.1)
+
+
 
 # Here's some blinkLED code if you need a Pymakr sanity check.
 # import machine #you can ignore the yellow underline here
